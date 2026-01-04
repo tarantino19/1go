1a36ec22c641ca2cd818935edae7541d641c9379
Red Tolentino <tolentinored19@gmail.com>
2026-01-04 17:24:21 +0800

    simple word counter algorithm

--- main.go
+++ main.go
@@ -10,6 +10,14 @@ func main() {
 	fmt.Println("Word Counter Begins...")
 	data, _ := os.ReadFile("./words.txt")
 
-	fmt.Println("data:", string(data))
+	wordCount := 0
 
+	for _, v := range data {
+		if v == ' ' {
+			wordCount++
+		}
+	}
+
+	wordCount++
+	fmt.Println(wordCount)
 }
